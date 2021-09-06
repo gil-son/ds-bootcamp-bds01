f88c59a772acaf1133a14eda33258f964e22ad27
“gil-son” <gilszon@gmail.com>
2021-09-06 20:58:43 -0300

    Repositories

---
+++ bds01/src/main/java/com/devsuperior/bds01/repositories/RepositoryEmployee.java
@@ -0,0 +1,12 @@
+package com.devsuperior.bds01.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devsuperior.bds01.entities.Department;
+
+@Repository
+public interface RepositoryEmployee extends JpaRepository<Department, Long> {
+
+	
+}

70c3a8e78ddd4f1a486546add6ad8b5a30906023
“gil-son” <gilszon@gmail.com>
2021-09-29 19:44:55 -0300

    TDD Employee - Pageable

--- bds01/src/main/java/com/devsuperior/bds01/services/EmployeeService.java
+++ bds01/src/main/java/com/devsuperior/bds01/services/EmployeeService.java
@@ -1,17 +1,32 @@
 package com.devsuperior.bds01.services;
 
+
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.bds01.dto.EmployeeDTO;
+import com.devsuperior.bds01.entities.Employee;
 import com.devsuperior.bds01.repositories.EmployeeRepository;
 
 @Service
 public class EmployeeService {
 
-	private EmployeeRepository employeeRepository;
+	private EmployeeRepository repository;
 
 	
-	EmployeeService(EmployeeRepository employeeRepository){
-		this.employeeRepository = employeeRepository;
+	EmployeeService(EmployeeRepository repository){
+		this.repository = repository;
 	}
+
 	
+	
+	public Page<EmployeeDTO> findAll(Pageable pegeable){
+		Page<Employee> page = repository.findAll(pegeable);
+		
+		return page.map(x -> new EmployeeDTO(x));
+	}
 }
